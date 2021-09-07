f3b4af566ee4c0e0c498057a413992e45f9a3d0c
Alexander Zveruk <alexander.zveruk@just-eat.com>
2021-09-08 00:36:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Hi bot claimer
+
+Claims HI dollars daily by answering bot's questions in chat
+
+# How to use
+
+1. Get telegram api token, insert it into `config.ini`.
+2. Install pipenv requirements `pipenv sync`
+3. Ensure you started conversation with HI Bot in Telegram
+4. Run script with `pipenv run python main.py <YOUR_ACCOUNT_NAME>`
---
+++ main.py
@@ -0,0 +1,59 @@
+import asyncio
+import sys
+
+from pyrogram import Client, filters
+from pyrogram.types import Dialog, Message
+
+HI_BOT = "hiofficialbot"
+
+name = sys.argv[1]
+if len(sys.argv) != 2:
+    print("Pass session name in arguments")
+    sys.exit(1)
+
+app = Client(name)
+
+
+claim_msg = "👋 Claim Daily Reward"
+
+
+async def find_hi_chat(app: Client) -> Dialog:
+    async for d in app.iter_dialogs():
+        if d.chat.username == HI_BOT and d.chat.type == "bot":
+            print(f"Hi chat id is {d.chat.id}")
+            return d
+
+
+async def main() -> None:
+    await app.start()
+
+    @app.on_message(filters.bot)
+    def claimer(client: Client, message: Message) -> None:
+        chat_id = message.chat.id
+        if message.chat.username != HI_BOT:
+            return
+        markup = message.reply_markup
+        if not markup:
+            return
+        keyboard = getattr(markup, "inline_keyboard", None)
+        if not keyboard:
+            return
+        option = keyboard[0][0]
+        data = option.callback_data
+        if not data or data == "NICKNAME_CB":
+            return
+        print(f"Callback answer is {option.text}")
+        rv = client.request_callback_answer(chat_id, message.message_id, data)
+        print(f"replied, {rv}")
+
+    hi = await find_hi_chat(app)
+    if not hi:
+        raise Exception("Cannot find HI bot, ensure you started converstaion with bot")
+    while True:
+        print("Sending claim message")
+        await app.send_message(hi.chat.id, claim_msg)
+        print("Sleeping for a day...")
+        await asyncio.sleep(60 * 60 * 24)
+
+
+app.run(main())
